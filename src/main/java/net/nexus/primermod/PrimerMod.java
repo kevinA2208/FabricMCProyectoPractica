77309f25fd65ff208b705cab714df3db77746c32
kevinA2208 <usamo489@gmail.com>
2022-05-01 14:39:25 -0500

    Primeros items creados

--- src/main/java/net/nexus/primermod/PrimerMod.java
+++ src/main/java/net/nexus/primermod/PrimerMod.java
@@ -1,15 +1,16 @@
 package net.nexus.primermod;
 
 import net.fabricmc.api.ModInitializer;
+import net.nexus.primermod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PrimerMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "primermod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItem();
 	}
 }
---
+++ src/main/java/net/nexus/primermod/item/ModItems.java
@@ -0,0 +1,32 @@
+package net.nexus.primermod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.nexus.primermod.PrimerMod;
+
+public class ModItems {
+
+    public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item MYTHRIL_NUGGET = registerItem("mythril_nugget",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item BLOOD_INGOT = registerItem("blood_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item FRIES_POTATO = registerItem("fries_potato",
+            new Item(new FabricItemSettings().group(ItemGroup.FOOD)));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registry.ITEM, new Identifier(PrimerMod.MOD_ID, name), item);
+    }
+    public static void registerModItem(){
+        PrimerMod.LOGGER.info("Registrando items de mod para"+ PrimerMod.MOD_ID);
+
+    }
+
+}
