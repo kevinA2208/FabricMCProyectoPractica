847cb427b5ffcbc7bae80ff1ed365b727dc3af43
kevinA2208 <usamo489@gmail.com>
2022-05-02 16:10:38 -0500

    Creación de loot tables, objetos que dropean al romperse un bloque, y los mineable y needs tool archivos de json

---
+++ src/main/java/net/nexus/primermod/item/custom/DowsingRodItem.java
@@ -0,0 +1,19 @@
+package net.nexus.primermod.item.custom;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.util.ActionResult;
+
+public class DowsingRodItem extends Item {
+    public DowsingRodItem(Settings settings){
+        super(settings);
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+
+
+
+        return super.useOnBlock(context);
+    }
+}
