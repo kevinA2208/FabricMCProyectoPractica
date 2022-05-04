0ebb551e6e516e4071fed0d4d063b1cbcbc780fe
kevinA2208 <usamo489@gmail.com>
2022-05-04 15:04:50 -0500

    Creacion de bloques no bloques (cerca, boton, placa de presión, muro, puerta de cerca) y sus archivos json

--- src/main/java/net/nexus/primermod/block/ModBlocks.java
+++ src/main/java/net/nexus/primermod/block/ModBlocks.java
@@ -2,8 +2,7 @@ package net.nexus.primermod.block;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.BlockItem;
@@ -18,6 +17,8 @@ import net.minecraft.world.World;
 import net.nexus.primermod.PrimerMod;
 import net.nexus.primermod.block.custom.SpeedyBlock;
 import net.nexus.primermod.item.ModItemGroup;
+import net.nexus.primermod.item.custom.ModPressurePlateBlock;
+import net.nexus.primermod.item.custom.ModStoneButtonBlock;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -38,6 +39,37 @@ public class ModBlocks {
 
     public static final Block SPEEDY_BLOCK = registerBlock("speedy_block",
             new SpeedyBlock(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()),ItemGroup.BUILDING_BLOCKS);
+
+    //ATENCION, una vez creado los bloques no bloques, es necesario crear los archivos json en blockstates, models/block y apartir de ahi se les da una textura, ya sea nueva
+    //o usandose una que ya exista, ya que solo se utiliza la textura, pero la forma del bloque es el que se le da en este archivo
+    //En los archivos blockstates de los bloques no bloques, se pueden copiar y pegar de los archivos de los bloques originales en los assets de minecraft
+    //y modificar al gusto
+
+    //se crea el mythril button, el boton customizado con mythril, que se basa en el esquema o estructura del boton original de piedra de minecraft
+    //para esto se pone que el bloque sea un bloque de tipo ModStoneButtonBlock, se pone que no tiene colision porque es un boton
+    public static final Block MYTHRIL_BUTTON = registerBlock("mythril_button",
+            new ModStoneButtonBlock(FabricBlockSettings.of(Material.METAL)
+                    .strength(6f).noCollision()),ModItemGroup.MYTHRIL);
+
+    //Se crea el mythril pressure plate, que se basa del pressure plate de minecraft, se pone que el bloque sea de tipo ModPressurePlateBlock
+    //Se le agrega el ActivationRule.EVERYTHING que es necesario, para que la plataforma de presion se active o se presione con cualquier objeto o entidad.
+    public static final Block MYTHRIL_PRESSURE_PLATE = registerBlock("mythril_pressure_plate",
+            new ModPressurePlateBlock(PressurePlateBlock.ActivationRule.EVERYTHING,
+                    FabricBlockSettings.of(Material.METAL)
+                    .strength(6f).requiresTool()),ModItemGroup.MYTHRIL);
+
+    //Se crean los fences o cercas, de mythril, que se construyen a partir de las cercas originales de madera de minecraft
+    public static final Block MYTHRIL_FENCE = registerBlock("mythril_fence", new FenceBlock(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()),ModItemGroup.MYTHRIL);
+
+    //Se crean los mismos bloques a partir de sus bloques originales de minecraft
+    public static final Block MYTHRIL_FENCE_GATE = registerBlock("mythril_fence_gate", new FenceGateBlock(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()),ModItemGroup.MYTHRIL);
+
+    public static final Block MYTHRIL_WALL = registerBlock("mythril_wall", new WallBlock(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()),ModItemGroup.MYTHRIL);
+
+
+
+
+
 
 
     //Se copian las mismas funciones de registrar bloques y item bloques, pero esta vez se les agrega un parametro nuevo
---
+++ src/main/java/net/nexus/primermod/item/custom/ModStoneButtonBlock.java
@@ -0,0 +1,11 @@
+package net.nexus.primermod.item.custom;
+
+import net.minecraft.block.StoneButtonBlock;
+
+//Con las funciones ModStoneButtonBlock, extienden de el bloque boton de piedra original de minecraft
+//por lo tanto nos da el esquema del objeto para poder basarnos en el con otros nuevos objetos
+public class ModStoneButtonBlock extends StoneButtonBlock {
+    public ModStoneButtonBlock(Settings settings) {
+        super(settings);
+    }
+}
