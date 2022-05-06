600f86fbe7018681e06f7b995da65d51779e06ca
kevinA2208 <usamo489@gmail.com>
2022-05-06 11:16:35 -0500

    Se crea una flor personalizada y su pot, y se crean los drops de los bloques con sus loot tables y sus recetas

--- src/main/java/net/nexus/primermod/PrimerClientMod.java
+++ src/main/java/net/nexus/primermod/PrimerClientMod.java
@@ -5,12 +5,14 @@ import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
 import net.nexus.primermod.block.ModBlocks;
 
-//Esta funcion es para que se muestren correctamente los bloques especiales que se vayan a hacer como leaves (hojas) puertas y bloques transparentes
+//Esta funcion es para que se muestren correctamente los bloques especiales que se vayan a hacer como leaves (hojas) puertas, flores y bloques transparentes
 public class PrimerClientMod implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.KAUPEN_DOOR, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.KAUPEN_TRAPDOOR, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LILAC_FLOWER, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.POTTED_LILAC_FLOWER, RenderLayer.getCutout());
     }
 }
--- src/main/java/net/nexus/primermod/block/ModBlocks.java
+++ src/main/java/net/nexus/primermod/block/ModBlocks.java
@@ -4,6 +4,8 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -24,7 +26,7 @@ public class ModBlocks {
 
     //BLOQUES COMUNES
     public static final Block MYTHRIL_BLOCK = registerBlock("mythril_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ModItemGroup.MYTHRIL, "tooltip.primermod.mythril_block");
+            new Block(FabricBlockSettings.of(Material.METAL).strength(4f).requiresTool()), ModItemGroup.MYTHRIL, "tooltip.primermod.mythril_block");
 
     public static final Block MYTHRIL_ORE = registerBlock("mythril_ore",
             new Block(FabricBlockSettings.of(Material.METAL).strength(4.5f).requiresTool()),ModItemGroup.MYTHRIL);
@@ -33,7 +35,7 @@ public class ModBlocks {
             new Block(FabricBlockSettings.of(Material.METAL).strength(4.5f).requiresTool()),ModItemGroup.MYTHRIL);
 
     public static final Block RAW_MYTHRIL_BLOCK = registerBlock("raw_mythril_block",
-            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()),ModItemGroup.MYTHRIL);
+            new Block(FabricBlockSettings.of(Material.METAL).strength(4f).requiresTool()),ModItemGroup.MYTHRIL);
 
 
     //BLOQUES ESPECIALES
@@ -53,40 +55,61 @@ public class ModBlocks {
 
     public static final Block MYTHRIL_BUTTON = registerBlock("mythril_button",
             new StoneButtonBlock(FabricBlockSettings.of(Material.METAL)
-                    .strength(6f).noCollision()),ModItemGroup.MYTHRIL);
+                    .strength(1f).noCollision()),ModItemGroup.MYTHRIL);
 
     //Se crea el mythril pressure plate, que se basa del pressure plate de minecraft, se pone que el bloque sea de tipo PressurePlateBlock
     //Se le agrega el ActivationRule.EVERYTHING que es necesario, para que la plataforma de presion se active o se presione con cualquier objeto o entidad.
     public static final Block MYTHRIL_PRESSURE_PLATE = registerBlock("mythril_pressure_plate",
             new PressurePlateBlock(PressurePlateBlock.ActivationRule.EVERYTHING,
                     FabricBlockSettings.of(Material.METAL)
-                    .strength(6f).requiresTool()),ModItemGroup.MYTHRIL);
+                    .strength(1f)),ModItemGroup.MYTHRIL);
 
     //Se crean los fences o cercas, de mythril, que se construyen a partir de las cercas originales de madera de minecraft
-    public static final Block MYTHRIL_FENCE = registerBlock("mythril_fence", new FenceBlock(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()),ModItemGroup.MYTHRIL);
+    public static final Block MYTHRIL_FENCE = registerBlock("mythril_fence", new FenceBlock(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()),ModItemGroup.MYTHRIL);
 
     //Se crean los mismos bloques a partir de sus bloques originales de minecraft
-    public static final Block MYTHRIL_FENCE_GATE = registerBlock("mythril_fence_gate", new FenceGateBlock(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()),ModItemGroup.MYTHRIL);
+    public static final Block MYTHRIL_FENCE_GATE = registerBlock("mythril_fence_gate", new FenceGateBlock(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()),ModItemGroup.MYTHRIL);
 
-    public static final Block MYTHRIL_WALL = registerBlock("mythril_wall", new WallBlock(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()),ModItemGroup.MYTHRIL);
-
-
-    public static final Block MYTHRIL_SLAB = registerBlock("mythril_slab", new SlabBlock(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()),ModItemGroup.MYTHRIL);
+    public static final Block MYTHRIL_WALL = registerBlock("mythril_wall", new WallBlock(FabricBlockSettings.of(Material.METAL).strength(4f).requiresTool()),ModItemGroup.MYTHRIL);
 
 
-    public static final Block MYTHRIL_STAIRS = registerBlock("mythril_stairs", new StairsBlock(ModBlocks.MYTHRIL_BLOCK.getDefaultState(), FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()),ModItemGroup.MYTHRIL);
+    public static final Block MYTHRIL_SLAB = registerBlock("mythril_slab", new SlabBlock(FabricBlockSettings.of(Material.METAL).strength(4f).requiresTool()),ModItemGroup.MYTHRIL);
+
+
+    public static final Block MYTHRIL_STAIRS = registerBlock("mythril_stairs", new StairsBlock(ModBlocks.MYTHRIL_BLOCK.getDefaultState(), FabricBlockSettings.of(Material.METAL).strength(4f).requiresTool()),ModItemGroup.MYTHRIL);
 
     //En la puerta como se supone que va a ser de madera, es necesario ponerle el Material.WOOD, para que sea de madera, si se pone Material.METAL será necesario usar redstone para abrirla
     //como una puerta de metal
     public static final Block KAUPEN_DOOR = registerBlock("kaupen_door", new DoorBlock(
-            FabricBlockSettings.of(Material.WOOD).strength(6f).requiresTool().nonOpaque()),ModItemGroup.MYTHRIL);
+            FabricBlockSettings.of(Material.WOOD).strength(4f).requiresTool().nonOpaque()),ModItemGroup.MYTHRIL);
 
     public static final Block KAUPEN_TRAPDOOR = registerBlock("kaupen_trapdoor", new TrapdoorBlock(
-            FabricBlockSettings.of(Material.WOOD).strength(6f).requiresTool().nonOpaque()),ModItemGroup.MYTHRIL);
+            FabricBlockSettings.of(Material.WOOD).strength(4f).requiresTool().nonOpaque()),ModItemGroup.MYTHRIL);
+
+
+    //FLORES Y POTS PARA FLORES
+    //Las flores se crean con el tipo de bloque Flower de Minecraft, este bloque necesita dos parametros
+    //el status effect que se le añade, y la duración de este efecto que es en segundos en esta ocasión
+    //el efecto que se le añade es cuando se craftea un suspicious stew que es un estofado de flores que da
+    //un efecto dependiendo de la flor, despues se le añade los mismos settings de bloque que la flor dandelion de minecraft
+    //y el nonOpaque se queda porque no es un bloque que nos choque
+    public static final Block LILAC_FLOWER = registerBlock("lilac_flower",
+            new FlowerBlock(StatusEffects.FIRE_RESISTANCE, 12,
+                    FabricBlockSettings.copy(Blocks.DANDELION).strength(0f).nonOpaque()),ItemGroup.MISC);
+
+    //en si este no es un bloque real, es solo para que la flor que creamos se pueda poner en los pots de minecraft que ya están
+    //Los pot para flores se crean con el tipo de bloque FlowerPotBlock, pero esta vez se llama una nueva funcion para registrar
+    //se llama registerBlockWithoutBlockItem, se llama con esta función ya que basicamente el pot es un bloque sin item, pero se le pone la flor cuando se le da click derecho
+    //tambien se pone que copie la estructura de Blocks.POTTED_ALLIUM de minecraft
+    public static final Block POTTED_LILAC_FLOWER = registerBlockWithoutBlockItem("potted_lilac_flower",
+            new FlowerPotBlock(ModBlocks.LILAC_FLOWER, FabricBlockSettings.copy(Blocks.POTTED_ALLIUM).nonOpaque()),ItemGroup.MISC);
 
 
 
     //CONSTRUCTORES
+
+
+
     //Se copian las mismas funciones de registrar bloques y item bloques, pero esta vez se les agrega un parametro nuevo
     //llamado toolTipKey, con esta key, se les asignará un mensaje de tip en el inventario
     private static Block registerBlock(String name, Block block, ItemGroup group, String toolTipKey){
@@ -103,6 +126,13 @@ public class ModBlocks {
                         tooltip.add(new TranslatableText(toolTipKey));
                     }
                 });
+    }
+
+
+    //La funcion de registerBlockWithoutBlockItem es la misma que registerBlock, solo que se le remueve el registerBlockItem que contiene
+    //ya que esta no tiene item, solo la flor que se le pone despues
+    private static Block registerBlockWithoutBlockItem(String name, Block block, ItemGroup group){
+        return Registry.register(Registry.BLOCK, new Identifier(PrimerMod.MOD_ID, name), block);
     }
 
 
